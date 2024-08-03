eda51124379eb105223580396d06413f01b16a47
martinrojasmet <97409637+martinrojasmet@users.noreply.github.com>
2024-08-03 17:57:50 -0400

    Improvements on terminal program & checking RIF format

--- test_main.py
+++ test_main.py
@@ -5,17 +5,46 @@ import requests
 import cv2
 import numpy as np
 import re
+import os
 
 base_url = "http://contribuyente.seniat.gob.ve/BuscaRif/"
+
+def check_format_rif(string):
+    pattern = r'^[VJ][0-9]{9}$'
+    result = False
+    if re.match(pattern, string):
+        result = True
+    return result
 
 def run(playwright: Playwright) -> None:
     keepGoing = True
     retries = 1
 
     # Get the RIF and Cedula
-    rif = input("Introduzca el RIF: ")
-    cedula = input("Introduzca la Cedula: ")
-    print("==============================")
+    from simple_term_menu import TerminalMenu
+
+    print("""
+CONSULTA DE RIF (SENIAT)
+
+Por favor, seleccione el tipo de ente que desea consultar:""")
+    options = ["Persona Natural", "Empresa"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+
+    rif_condition = False
+    while not rif_condition:
+        rif = input("Introduzca el RIF: ")
+        rif_condition = check_format_rif(rif)
+        os.system('cls||clear')
+        if not rif_condition:
+            print("El formato del RIF es incorrecto. Por favor, intente de nuevo.")
+
+    id = ""
+    if menu_entry_index == 0:
+        id = input("Introduzca la Cedula o Pasaporte: ")
+    os.system('cls||clear')
+
+    print ("Abriendo la pagina...")
 
     # Go to page
     browser = playwright.chromium.launch(headless=False)
@@ -27,10 +56,14 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto(base_url + "BuscaRif.jsp")
 
+    os.system('cls||clear')
+    print ("""Intentando acceder...
+""")
+
     while(keepGoing):
         # Fill Form
         page.get_by_role("textbox", name="Ingrese su número de Rif, seg").fill(rif)
-        page.get_by_role("textbox", name="Ingrese su número de Cédula o").fill(cedula)
+        page.get_by_role("textbox", name="Ingrese su número de Cédula o").fill(id)
 
         # Download Image
         page.get_by_role("img").screenshot(path="tmp/captcha.jpg")
@@ -78,34 +111,37 @@ def run(playwright: Playwright) -> None:
             # Get the percentage of retention
             retention_percentage = re.search(r'retención del (\d+)%', text)
 
-            print("==============================")
+            os.system('cls||clear')
+            print("RESULTADOS")
             print()
 
             # Print the name of the company/person
-            print(f"Nombre: {name}")
-            print()
+            print(f"Nombre del ente: {name}")
+            print("""
+Detalles:""")
 
             # Print if the person is a special contributor
-            if("Contribuyente Ordinario del IVA" in text):
+            if("- Contribuyente Ordinario del IVA" in text):
                 print("Es contribuyente ordinario del IVA")
 
             # Print if the person is a formal contributor
-            if("Contribuyente Formal del IVA" in text):
+            if("- Contribuyente Formal del IVA" in text):
                 print("Es contribuyente formal del IVA")
 
             # Print if the person is an agent of retention
-            if("Agente de Retención del IVA" in text):
+            if("- Agente de Retención del IVA" in text):
                 print("Es agente de retención del IVA")
 
             # Print the percentage of retention
             print()
             if(retention_percentage):
-                print(f"Porcentaje de retención: {retention_percentage.group(1)}%")
+                print(f"- Porcentaje de retención: {retention_percentage.group(1)}%")
             else:
-                print("No requiere retención")
+                print("- No requiere retención")
 
     context.close()
     browser.close()
 
 with sync_playwright() as playwright:
     run(playwright)
+
